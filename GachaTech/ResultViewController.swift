e6bf97ecb52ce91dc31fad4f283fe70129723499
Hinako Nakai <hinako.0113ngmail.com>
2022-05-14 21:40:10 +0900

    gacha

---
+++ GachaTech/ResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ResultViewController.swift
+//  GachaTech
+//
+//  Created by 中井日向子 on 2022/05/12.
+//
+
+import Foundation
--- GachaTech/ViewController.swift
+++ GachaTech/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var monsterImageView: UIImageView!
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+    var number: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setImage()
+    }
+    
+    func setImage() {
+        number = Int.random(in: 0...9)
+        if number == 0 {
+            monsterImageView.image = UIImage(named: "monster010")
+            backgroundImageView.image = UIImage(named: "bg_gold")
+        } else if number == 1 {
+            monsterImageView.image = UIImage(named: "monster008")
+            backgroundImageView.image = UIImage(named: "bg_gold")
+        } else if number < 5 {
+            monsterImageView.image = UIImage(named: "monster006")
+            backgroundImageView.image = UIImage(named: "bg_red")
+        } else if number < 7 {
+            monsterImageView.image = UIImage(named: "monster004")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        } else {
+            monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+    }
+    
+    @IBAction func back() {
+        self.dismiss(animated: true, completion: nil)
     }
 
-
 }
-
